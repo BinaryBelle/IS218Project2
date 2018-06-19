89a828cbaafba2567dba631ac95d3372cb2b91d4
BinaryBelle <tc329@njit.edu>
2018-06-19 18:17:10 +0000

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,22 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <title> Owl City </title>
+   <body>
+      <div class="nav">
+         <div class="container">
+            <ul class="pull-left">
+               <li><a href="#">Home</a></li>
+               <li><a href="#">About</a></li>
+               <li><a href="#">Contact</a></li>
+            </ul>
+            <ul class= "pull-right">
+               <li><a href="#">Sign In/Sign Up</a></li>
+               <li><a href="#">View Messages</a></li>
+               <li><a href="#">Logout</a></li>
+         </div>
+       </div>
+            
+   </body>
 
 </head>
 </html>
